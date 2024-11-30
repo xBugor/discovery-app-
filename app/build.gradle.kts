98bd99516d0d8f1f9d49425a0d12dbf327e7d791
BugOR <145480350+mrBayCE@users.noreply.github.com>
2024-12-01 00:02:18 +0300

    Revert "güncelleme"
    This reverts commit 495b9a7d968709c709837263f66c114a27b8cb4c.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -43,7 +42,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
