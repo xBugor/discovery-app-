a800daf0c5eb3bddcd18eeaa3a07feb743171a92
BugOR <145480350+mrBayCE@users.noreply.github.com>
2024-12-02 21:32:41 +0300

    çalışmagüncelleme

--- app/src/main/java/com/example/mobileprogramming/KaydolmaEkran.kt
+++ app/src/main/java/com/example/mobileprogramming/KaydolmaEkran.kt
@@ -1,31 +1,28 @@
 package com.example.mobileprogramming
+package com.codingstuff.loginandsignup
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.InputBinding
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.codingstuff.loginandsignup.databinding.ActivitySignUpBinding
 
-class MainActivity : AppCompatActivity() {
-
+class KaydolmaEkran : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_kaydolma_ekran)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-        }
-        val kayit=findViewById<Button>(R.id.kayit)
-        kayit.setOnClickListener {
-            val intent = Intent(this, KaydolmaEkran::class.java)
-            startActivity(intent)
         }
     }
 }
