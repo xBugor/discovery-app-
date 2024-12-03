794add2f36040ab09cbe85f325f1f480a0cbe9b0
BugOR <145480350+mrBayCE@users.noreply.github.com>
2024-12-03 19:10:07 +0300

    Kayıtolmatamamlandı

--- app/src/main/java/com/example/mobileprogramming/KaydolmaEkran.kt
+++ app/src/main/java/com/example/mobileprogramming/KaydolmaEkran.kt
@@ -1,28 +1,77 @@
 package com.example.mobileprogramming
-package com.codingstuff.loginandsignup
 
-import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import android.widget.Toast
-
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
-import com.codingstuff.loginandsignup.databinding.ActivitySignUpBinding
 
 class KaydolmaEkran : AppCompatActivity() {
+
+    private lateinit var firebase: FirebaseAuth//firebase kontrolü için nesne değişkenin atanması
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContentView(R.layout.activity_kaydolma_ekran)
+        firebase = FirebaseAuth.getInstance()
+        val kayitbuton=findViewById<Button>(R.id.kayitolbuton)
+        val mailgir=findViewById<TextInputLayout>(R.id.mailLayout)
+        val sifregir=findViewById<TextInputLayout>(R.id.sifrelayout)
+        val sifrekontrol=findViewById<TextInputLayout>(R.id.sifredogrulalayout)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        kayitbuton.setOnClickListener{
+            val email = mailgir.editText?.text.toString()
+            val sifre =sifregir.editText?.text.toString()
+            val sifreKontrolString=sifrekontrol.editText?.text.toString()
+            if (email.isNotEmpty() && sifre.isNotEmpty() && sifreKontrolString.isNotEmpty()) {
+                if (sifre == sifreKontrolString) {
+                    firebase.createUserWithEmailAndPassword(email, sifre).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Snackbar.make(
+                                findViewById(android.R.id.content),
+                                "Kayıt Tamamlandı!",
+                                Snackbar.LENGTH_SHORT
+                            ).show();
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+
+                        else {
+                            Snackbar.make(findViewById(android.R.id.content), "Kayıt Başarısız: " , Snackbar.LENGTH_LONG)
+                                .show();
+
+                        }
+
+                    }
+
+
+                } else {
+                    Toast.makeText(this, "Şifreler uyuşmuyor", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else {
+                Toast.makeText(this, "İlgili alanları doldurunuz", Toast.LENGTH_SHORT).show()
+
+            }
+
+
+
+
+        }
+
     }
 }
