fcff1b6e0b72fa4aea22885a39ae85a03d73427a
BugOR <145480350+mrBayCE@users.noreply.github.com>
2024-12-08 12:21:12 +0300

    draweryapıldı

--- app/src/main/java/com/example/mobileprogramming/Giris.kt
+++ app/src/main/java/com/example/mobileprogramming/Giris.kt
@@ -1,13 +1,19 @@
 package com.example.mobileprogramming
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class Giris : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+         lateinit var drawerLayout: DrawerLayout
+
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_giris)
@@ -16,5 +22,15 @@ class Giris : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        drawerLayout=findViewById(R.id.main)
+        var menubutton=findViewById<ImageButton>(R.id.menu)
+
+        menubutton.setOnClickListener(){
+            drawerLayout.open()
+        }
+
+
     }
+
+
 }
