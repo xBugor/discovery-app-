735a8092168cd4a2ce057d7a312bb961314edbfa
BugOR <145480350+mrBayCE@users.noreply.github.com>
2024-12-09 19:44:55 +0300

    Revert "Aramacubuğu eklendi"
    This reverts commit fc980e84396136e03a1722c6fbab9401df2b83a3.

--- app/src/main/java/com/example/mobileprogramming/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.kt
@@ -77,6 +77,15 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
