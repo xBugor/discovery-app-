c643392eb486bf64140c8780833a4586c5db2dc6
BugOR <145480350+mrBayCE@users.noreply.github.com>
2024-12-09 19:48:33 +0300

    Reapply "Aramacubuğu eklendi"
    This reverts commit ec2ce05ff15db5bd40be959cc333c7821b9cf46e.

--- app/src/main/java/com/example/mobileprogramming/Giris.kt
+++ app/src/main/java/com/example/mobileprogramming/Giris.kt
@@ -1,16 +1,20 @@
 package com.example.mobileprogramming
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class Giris : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
          lateinit var drawerLayout: DrawerLayout
+         lateinit var navigationView: NavigationView
 
 
 
@@ -24,9 +28,33 @@ class Giris : AppCompatActivity() {
         }
         drawerLayout=findViewById(R.id.main)
         var menubutton=findViewById<ImageButton>(R.id.menu)
+        navigationView=findViewById(R.id.cekmece)
 
         menubutton.setOnClickListener(){
             drawerLayout.open()
+        }
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.Eposta ->{
+                    Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navgorus ->{
+                    Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navayarlar ->{
+                    val intent = Intent(this, Ayarlar::class.java)
+                    startActivity(intent)
+
+                }
+                R.id.navcikis ->{
+                    Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+            // Tıklamadan sonra çekmeceyi kapat
+            drawerLayout.closeDrawers()
+            true
         }
 
 
